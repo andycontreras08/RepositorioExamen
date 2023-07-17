ee6d5aaed9b9ba2e34ff889d855f101d13c5f74d
andycontreras08 <dsm20190452.jhernandez@alumnos.utsv.edu.mx>
2023-07-17 16:29:05 -0600

    Commit en el que se borró el archvio Registrarse.java del proyecto.

--- Registrarse.java
+++
@@ -1,8 +0,0 @@
-public class registrarse {
-    public void registrarse(Request request) {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        System.out.println("Registro exitoso para el usuario: " + username);
-    }
-}
