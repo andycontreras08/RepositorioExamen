607eb8225ea940aa26e22d3985f80bec288d291a
andycontreras08 <dsm20190452.jhernandez@alumnos.utsv.edu.mx>
2023-07-17 16:21:11 -0600

    Commit en el que se encuentra el login principal, que recibe los parámetros de username y password, para autenticarse, y en dado caso de coincidir envía un mensaje de Inicio de sesión exitoso, en caso contrario, enviará un mensaje de Credenciales inválidas

---
+++ Login.java
@@ -0,0 +1,13 @@
+public class Login{
+    public void login (Request request){
+        //Aquí va el código que recoge las credenciales del usuario
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("usuario") && password.equals("contraseña")) {
+            System.out.println("Inicio de sesión exitoso");
+        } else {
+            System.out.println("Credenciales inválidas");
+    }
+    }
+}
